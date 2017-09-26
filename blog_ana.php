9470d32ef327647fd8d98687d7eb5ffef20be078
allen <allen.fantasy@gmail.com>
2017-09-26 10:20:51 +0800

    fix blog_ana bug

--- blog_ana.php
+++ blog_ana.php
@@ -27,7 +27,7 @@ $(document).ready(function() {
 <body>
 
 <div>
-<a href="?dd=today">本月</a> 
+<a href="?dd=today" target="_self">本月</a> 
 <?php
 $mm = 8;
 $tmm = intval(date('m'));
@@ -36,7 +36,7 @@ while ($mm< $tmm) {
     $imm = ($mm-1) % 12 + 1;
     //$imm = str_pad($imm,2,"0",STR_PAD_BOTH);
     $imm = $yy * 100 + $imm;
-    echo '<a href="?dd='.$imm.'">'.$imm.'</a>';
+    echo '<a href="?dd='.$imm.'" target="_self">'.$imm.'</a>';
 
     $mm ++;
 } 
@@ -107,11 +107,10 @@ mysql_select_db(DB_NAME, $con);
 
 
 //如果上个月的没有统计就进行统计
-$sql ="select id,user_id from post_detail where yymm=date_format(date_sub(now(), interval 1 day),'%Y%m') limit 2";
+$sql ="select id,user_id from post_detail where yymm=date_format(date_sub(now(), interval 1 month),'%Y%m') limit 1";
 $result = mysql_query($sql);
-//echo $result;
 if (!$result || mysql_num_rows($result) == 0) {
-  echo "last month has data!";
+  echo "last month has no data!< /br>";
 
     $sql = "
     insert into post_detail ( yymm,post_id, title, user_id,username, nicename,truename,iswrite,views,comments,loves)
@@ -122,7 +121,7 @@ if (!$result || mysql_num_rows($result) == 0) {
     u.user_login as username,
     u.user_nicename as nicename,
             (select group_concat(um.meta_value separator ' ') from wp_usermeta um where um.user_id=p.post_author and (um.meta_key = 'first_name' or um.meta_key='last_name')) as truename,
-    case when instr(p.post_title,'【原创')=1 then 1  else 0 end as iswrite,
+    case when instr(p.post_title,'【原创')=1 then 2 when instr(p.post_title,'【转载')=1 then 1 else 0 end as iswrite,
     pm.meta_value as views,
     p.comment_count as comments,
     0 as loves
@@ -137,23 +136,24 @@ if (!$result || mysql_num_rows($result) == 0) {
 
 }
 
-$q_date = $_GET['dd'];
+$q_date = isset($_GET['dd'])?trim($_GET['dd']):"today";
 $q_date = mysql_real_escape_string($q_date);
 echo "<p>". $q_date. "</p>";
 if($q_date == "today") {
 
-    $sql = "update wp_posts set post_title=replace(post_title,'【分享】','【转载】') where post_title like '【分享】%' and post_date > concat(date_format(now(), Y-%m'),'-01') ";
-    mysql_query($sql);
+    //$sql = "update wp_posts set post_title=replace(post_title,'【分享】','【转载】') where post_title like '【分享】%' and post_date > concat(date_format(now(), Y-%m'),'-01') ";
+    //mysql_query($sql);
 
     $sql = "
     select user_id ,concat('<a href=\"/p/author/',username, '\">',nicename,'(',truename,')</a>') as author ,
- sum(iswrite)  as writes ,count(*) - sum(iswrite) as copys, sum(comments) as comments,sum(views) as views from 
+ sum(case iswrite when 2 then 1 else 0 end)  as writes ,sum(case iswrite when 1 then 1 else 0 end) as copys,count(*) as posts,
+ sum(comments) as comments,sum(views) as views from 
     (
      select u.ID as user_id,
     u.user_login as username,
     u.user_nicename as nicename,
             (select group_concat(um.meta_value separator ' ') from wp_usermeta um where um.user_id=p.post_author and (um.meta_key = 'first_name' or um.meta_key='last_name')) as truename,
-    case when instr(p.post_title,'【原创')=1 then 1  else 0 end as iswrite,
+    case when instr(p.post_title,'【原创')=1 then 2 when instr(p.post_title,'【转载')=1 then 1 else 0 end as iswrite,
     pm.meta_value as views,
     p.comment_count as comments,
     0 as loves
@@ -172,10 +172,10 @@ if($q_date == "today") {
     $data1=mysql_query($sql);
 
     $sql = "
-     select concat('<a href=\"p/',p.ID,'\">',p.post_title,'</a>') as title,
+     select concat('<a href=\"p/',p.ID,'\">',left(p.post_title, 80),'</a>') as title,
      u.ID as user_id,
             (select group_concat(um.meta_value separator ' ') from wp_usermeta um where um.user_id=p.post_author and (um.meta_key = 'first_name' or um.meta_key='last_name')) as truename,
-    case when instr(p.post_title,'【原创')=1 then 1  else 0 end as iswrite,
+    case when instr(p.post_title,'【原创')=1 then 'W' when instr(p.post_title,'【转载')=1 then 'S' else 'O' end as fromwhere,
     pm.meta_value as views,
     p.comment_count as comments,
     0 as loves
@@ -184,20 +184,24 @@ if($q_date == "today") {
             inner join wp_postmeta pm on pm.post_id=p.ID and pm.meta_key='views'
             
             where p.post_author>1 and p.post_type='post' and p.post_status='publish' and p.post_date between concat(date_format(now(),'%Y-%m'),'-01') and concat(date_format(date_sub(now(), interval -1 month),'%Y-%m'),'-01')
-order by iswrite desc,views desc ,comments desc
+order by views desc ,comments desc
     ";
     $data2=mysql_query($sql);
+
 } else {
 
     $sql = "
-select user_id, concat('<a href=\"/p/author/',username, '\">',nicename,'(',truename,')</a>') as author, sum(iswrite)  as writes ,count(*) - sum(iswrite) as copys, sum(comments) as comments,sum(views) as views from 
+select user_id, concat('<a href=\"/p/author/',username, '\">',nicename,'(',truename,')</a>') as author, 
+sum(case iswrite when 2 then 1 else 0 end)  as writes ,sum(case iswrite when 1 then 1 else 0 end) as copys,count(*) as posts,
+sum(comments) as comments,sum(views) as views from 
 post_detail where yymm=${q_date} group by user_id,username,nicename,truename
  order by writes desc,views desc,copys desc
 ";
     $data1 = mysql_query($sql);
     
 
-    $sql = "select concat('<a href=\"p/',post_id,'\">',title,'</a>') as title,concat('<a href=\"/p/author/',username, '\">',nicename,'(',truename,')</a>') as author,case when iswrite=1 then 'W' else 'C' end as fromwhere,views,comments,loves from post_detail where yymm=${q_date} 
+    $sql = "select concat('<a href=\"p/',post_id,'\">',left(title,80),'</a>') as title,concat('<a href=\"/p/author/',username, '\">',nicename,'(',truename,')</a>') as author,
+        case iswrite when 2 then 'W' when 1 then 'S' else 'O' end as fromwhere,views,comments,loves from post_detail where yymm=${q_date} 
         order by iswrite desc,views desc,comments desc,loves desc";
     $data2 = mysql_query($sql);
 
@@ -212,69 +216,6 @@ mysql_close($con);
 
 
 /*
-select u.ID, u.user_nicename, count(*) from wp_posts p inner join wp_users u on p.post_author=u.ID where post_author>1 and post_type='post' and post_status='publish' and post_date between '2017-08-01' and  '2017-09-01' group by u.ID,u.user_nicename;
-
-
-
-
-select u.ID, u.user_nicename, count(*) from wp_posts p inner join wp_users u on p.post_author=u.ID where post_author>1 and post_type='post' and post_status='publish' and post_date between '2017-08-01' and  '2017-09-01' and post_title like '【原创%' group by u.ID,u.user_nicename;
-
-
-
-select u.ID, u.user_nicename, count(*) from wp_posts p inner join wp_users u on p.post_author=u.ID where post_author>1 and post_type='post' and post_status='publish' and post_date between '2017-08-01' and  '2017-09-01' and post_title like '【转载%' group by u.ID,u.user_nicename;
-
-
-
-
-select u.ID, u.user_nicename, count(*) as co, sum(comment_count) as comments from wp_posts p inner join wp_users u on p.post_author=u.ID where post_author>1 and post_type='post' and post_status='publish' and post_date between '2017-08-01' and  '2017-09-01' and post_title like '【原创%' group by u.ID,u.user_nicename;
-
-
-
-
-insert into post_detail (ID, yymm,post_id, title, user_id,nicename,truename,iswrite,views,comments,love)
- select ,
-         p.ID as post_id,
-p.post_title as title,
- u.ID as user_id,
-u.user_nicename,
-        (select group_concat(um.meta_value separator ' ') from wp_usermeta um where um.user_id=p.post_author and (um.meta_key = "first_name" or um.meta_key='last_name')) as truename,
-case when instr(p.post_title,'【原创')=1 then 1  else 0 end as iswrite,
-pm.meta_value as views,
-p.comment_count as comments,
-0 as loves
- from wp_posts p
-        inner join wp_users u on p.post_author=u.ID 
-        inner join wp_postmeta pm on pm.post_id=p.ID and pm.meta_key='views'
-        
-        where p.post_author>1 and p.post_type='post' and p.post_status='publish' and p.post_date between '2017-08-01' and  '2017-09-01'  
-        
-
-
- select date_format(now(),'%Y-%m'),
-         p.ID as post_id,
-p.post_title as title,
- u.ID as user_id,
-u.user_nicename,
-        (select group_concat(um.meta_value separator ' ') from wp_usermeta um where um.user_id=p.post_author and (um.meta_key = "first_name" or um.meta_key='last_name')) as truename,
-case when instr(p.post_title,'【原创')=1 then 1  else 0 end as iswrite,
-pm.meta_value as views,
-p.comment_count as comments,
-0 as loves
- from wp_posts p
-        inner join wp_users u on p.post_author=u.ID 
-        inner join wp_postmeta pm on pm.post_id=p.ID and pm.meta_key='views'
-        
-        where p.post_author>1 and p.post_type='post' and p.post_status='publish' and p.post_date between concat(date_format(now(),'%Y-%m'),'-01') and concat(date_format(date_sub(now(), interval -30 day,'%Y-%m'),'-01')
-
-
-
-
-select user_id, user_nicename, count(*) as posts,sum(iswrite)  as writes ,count(*) - sum(iswrite) as copys, sum(comment_count) as comments,sum(views) as views from (
-        
-       ) a  group by user_id,user_nicename;
-
-
-
 CREATE TABLE `post_detail` (
   `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `yymm` bigint(20) unsigned NOT NULL default '0',
